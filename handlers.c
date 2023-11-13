3da5aeafb98fc18a3479412db16f9e51ed057984
anas-shunsui <alas.anass1@gmail.com>
2023-11-13 21:16:38 +0100

    Non interactive mode

---
+++ handlers.c
@@ -0,0 +1,70 @@
+#include "simple_shell.h"
+
+/**
+ * _exit_status - Handles exit status
+ * @my_stat: The status to check.
+ * @sngl_cmd: The command arguments.
+ * @token: Token.
+ * @status: status
+ *
+ * Return: void.
+ */
+
+void _exit_status(int my_stat, char **sngl_cmd, char **token,
+											int *status)
+{
+	if (my_stat == -1 ||
+		(my_stat == 0 && sngl_cmd[1][0] != '0') ||
+		my_stat < 0)
+	{
+		write_exit_error(sngl_cmd[1]);
+		*status = 2;
+	}
+	else
+	{
+		free(*token);
+		free_array(sngl_cmd);
+		exit(my_stat);
+	}
+}
+
+/**
+ * _custom_exit - Handles custom exit statuses
+ * @my_stat: The exit status
+ * @cmds: command and its arguments.
+ * @l: the input line.
+ * @status: the return status value.
+ *
+ * Return: void.
+ */
+
+void _custom_exit(int my_stat, char **cmds,
+						char *l, int *status)
+{
+	if (my_stat == -1 ||
+		(my_stat == 0 && cmds[1][0] != '0') ||
+		my_stat < 0)
+	{
+		write_exit_error(cmds[1]);
+		*status = 2;
+	}
+	else
+	{
+		free(l);
+		free_array(cmds);
+		exit(my_stat);
+	}
+}
+/**
+ * _getline_error - Handles getline error.
+ * @l: the input line.
+ *
+ * Return: void.
+ */
+void _getline_error(char *l)
+{
+	perror("getline");
+	free(l);
+	l = NULL;
+	exit(EXIT_FAILURE);
+}
--- main.c
+++ main.c
@@ -1,32 +1,105 @@
 #include "simple_shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @argv: arg vector
+ * main - Entry point
  *
- * Return: Exit status
+ * Return: Returns 0 on success
  */
-int main(int ac, char **argv)
+
+int main(void)
 {
-	char *l = NULL, **cmd = NULL;
-	int stat = 0;
-	(void) ac;
+	size_t sizeOf_line = 0;
+	char *l = NULL;
+	int status = 0;
 
-	while (1)
+	if (!isatty(0))
 	{
-		l = read_line();
-		if (l == NULL)
+		while (getline(&l, &sizeOf_line, stdin) != -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			return (stat);
+			non_interactive_mode(l, &status);
 		}
-
-		cmd = splinter(l);
-		if (!cmd)
-			continue;
-
-		stat = _exec(cmd, argv);
+		if (l)
+		{
+			free(l);
+			l = NULL;
+		}
+		return (status);
 	}
+	debut_shell();
+	return (0);
+}
+
+/**
+ * non_interactive_mode - Executes shell commands in non_nteractive_mode
+ * @token: The string containing commands separated by newline characters
+ * @status: integer store the number
+ * Return: Returns status.
+ */
+void non_interactive_mode(char *token, int *status)
+{
+	char **single_command;
+	char *envp[] = {NULL};
+
+	token[strlen(token) - 1] = '\0';
+	single_command = tokenize_string(token, " \t");
+	if (single_command[0])
+	{
+		if (!_strcmp(single_command[0], "exit"))
+		{
+			if (single_command[1])
+			{
+				int my_status = _atoi(single_command[1]);
+
+				handle_exit_status(my_status, single_command, &token, status);
+			}
+			else
+			{
+				free(token);
+				free_array(single_command);
+				exit(*status);
+			}
+		}
+		else if (!_strcmp(single_command[0], "env"))
+		{
+			print_env_var();
+			*status = 0;
+		}
+		else
+		_execvep(single_command, envp, status);
+	}
+	free_array(single_command);
+}
+
+/**
+ * tokenize_string - Splits a string into tokens
+ * @str: The string to tokenize
+ * @delimiters: The delimiters to use for tokenization
+ *
+ * Return: Returns result.
+ */
+char **tokenize_string(char *str, char *delimiters)
+{
+	int count = 0;
+	char *token;
+	char **result = malloc(20 * sizeof(char *));
+
+	if (result == NULL)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(str, delimiters);
+	while (token != NULL)
+	{
+		result[count] = _strdup(token);
+		count++;
+		token = strtok(NULL, delimiters);
+	}
+	while (count < 20)
+	{
+		result[count] = NULL;
+		count++;
+	}
+
+	return (result);
 }
