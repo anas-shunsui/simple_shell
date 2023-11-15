2138c4b53fb8de9983a247538fcf35da26eca0fe
anas-shunsui <alas.anass1@gmail.com>
2023-11-15 16:48:16 +0100

    Shell's Header

--- simple_shell.h
+++ simple_shell.h
@@ -1,17 +1,17 @@
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
 
-#include<stdlib.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<stddef.h>
-#include<string.h>
-#include<sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <string.h>
+#include <sys/wait.h>
 #include <limits.h>
 
 extern char **environ;
 
-/* built function*/
+/* Functions */
 void _debut(void);
 void non_intermode(char *token, int *status);
 ssize_t read_cmd(char **l, size_t *sizeof_l);
@@ -20,23 +20,23 @@ int *status);
 void _custom_exit(int my_stat, char **cmds,
 char *l, int *status);
 
-/* execute command*/
+/* execute */
 void _exec(char **cmds, char **environ, int *status);
 int _search(char *cmd, char **path);
 void child_process(int *status);
 char *_getenv(const char *path);
 int _atoi(char *str);
 
-/* handle line and delimiters(/n...)*/
+/* toknizer and delimiters */
 char **splinter_string(char *s, char *del);
 
-/* write messages */
+/* Error messages */
 void _werror(char *cmd);
 void _wexit_werror(char *num);
 void _environ_var(void);
 void _getline_error(char *l);
 
-/* string fucntion*/
+/* strings*/
 char *_strdup(const char *src);
 int _strlen(char *str);
 int _strcmp(char *str1, char *str2);
@@ -44,7 +44,7 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 
 
-/* free memory leak*/
+/* memory leak*/
 void free_array(char **array);
 
 #endif
